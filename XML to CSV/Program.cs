bcf0a5ab990f85ed8d90341fefc7873d26360eb9
Marko Dosen <marko.dosen1@gmail.com>
2015-07-21 10:54:09 +0200

    Added some comments to Main

--- XML to CSV/Program.cs
+++ XML to CSV/Program.cs
@@ -10,13 +10,14 @@ namespace XML_to_CSV
     {
         static void Main(String[] arg)
         {
-            
+            //list of attributes that we need to display
             LinkedList<String> list = new LinkedList<String>();
             list.AddFirst("Subtotal");
             list.AddLast("Shipping");
             list.AddLast("Tax");
             list.AddLast("Payment Amount");
 
+            //if user doesn't put file path as argument, ask him to put it.
             Converter c = new Converter(list);
             if (arg.Length < 1)
             {
@@ -26,8 +27,10 @@ namespace XML_to_CSV
             else { 
                 c.filePath = arg[0];
             }
-            
+            //readFile to check if it is ok
             c.readFile();
+
+            //create scv file from file that was red
             c.fileCreate();
             
         }
